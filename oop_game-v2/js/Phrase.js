29c80c346406cae86b500c59efb8576722913bbe
Tristan Ringer <rin9er@gmail.com>
2024-11-22 16:23:20 -0700

    Create phrase class

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+
+// Create phrase class
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    addPhraseToDisplay() {
+        const phraseCharacters = this.phrase.split('');
+        const ul = document.querySelector('ul');
+
+        phraseCharacters.forEach(character => {
+            
+            const newLi = document.createElement('li');
+
+            newLi.classList.add('letter');
+            newLi.textContent = character;
+
+            ul.appendChild(newLi);
+        });
+    }
+};
+
