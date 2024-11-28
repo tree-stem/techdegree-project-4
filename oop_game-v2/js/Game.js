844478283887e4008f15cd64ed9f48f3f4f0666a
Tristan Ringer <rin9er@gmail.com>
2024-11-28 11:15:03 -0700

    Phrase class complete

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,65 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+import { Phrase } from './Phrase.js';
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [new Phrase ('NO PLACE LIKE HOME'), new Phrase ('MANS BESTFRIEND'), new Phrase ('NEW YEARS RESOLUTION'), new Phrase ('BEAT OF A DRUM'), new Phrase ('LEAVE ROOM FOR DESSERT')];
+        this.activePhrase = null;
+    }
+
+    getRandomPhrase() {
+        const randomNumber = Math.floor(Math.random() * 5);
+        this.activePhrase = this.phrases[randomNumber];
+
+        console.log('Random phrase:', this.activePhrase);
+
+        return this.activePhrase;
+    }
+
+    startGame() {
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'none';
+
+        this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+        this.activePhrase.checkLetter();    
+    }
+
+    handleInteraction() {
+        const keys = document.querySelectorAll('.keyrow .key');
+        const filteredPhrase = this.activePhrase.phrase.split('').filter(character => character !== ' ');
+   
+        keys.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const clickedKey = e.target.textContent.toLowerCase();
+                let match = false;
+
+                for ( let i = 0; i < filteredPhrase.length; i++ ) {
+                    
+                    match = false;
+
+                    if (clickedKey === filteredPhrase[i]) {
+                        item.classList.add('chosen');
+                        this.activePhrase.showMatchedLetter();
+                        item.disabled = true;
+                        match = true;
+                        break;
+                    }; 
+                };
+
+                if (!match) {
+                    item.classList.add('wrong');
+                    item.disabled = true;
+                };
+            });
+        });
+    };   
+};
+
+const test = new Game;
+test.startGame();
+test.handleInteraction();
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,9 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Phrase.js */
-
-
-// Create phrase class
+ * Game.js */
 
 class Phrase {
     constructor(phrase) {
@@ -12,17 +9,73 @@ class Phrase {
 
     addPhraseToDisplay() {
         const phraseCharacters = this.phrase.split('');
-        const ul = document.querySelector('ul');
+        const phraseDiv = document.querySelector('#phrase');
+        const ul = document.querySelector('#phrase ul');
+        phraseDiv.style.display = 'block';
 
+        ul.innerHTML = '';
+        
         phraseCharacters.forEach(character => {
+            const li = document.createElement('li');
+
+            if (character === ' ') {
+                li.classList.add('space');
+            } else {
+                li.classList.add('letter');
+                li.textContent = character;
+            };
+
+            ul.appendChild(li);
             
-            const newLi = document.createElement('li');
-
-            newLi.classList.add('letter');
-            newLi.textContent = character;
-
-            ul.appendChild(newLi);
         });
-    }
+    }; 
+
+    checkLetter() {
+        const keys = document.querySelectorAll('.keyrow .key');
+        const phraseCharacters = this.phrase.split('');
+        const noSpacePhraseCharacters = phraseCharacters.filter((character, index) => character !== ' ');
+        let match = false;
+
+        keys.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const clickedKey = e.target.textContent.toLowerCase();
+
+                match = false;
+                
+                for (let i = 0; i < noSpacePhraseCharacters.length; i++) {
+                    
+                    if (clickedKey === noSpacePhraseCharacters[i]) {
+                        console.log('Match found');
+                        match = true;
+                        break;
+                    };
+                };
+                    
+                if (!match) {
+                    console.log('Please try another key');
+                };            
+            });
+        });
+    };
+    
+    showMatchedLetter() {
+        const keys = document.querySelectorAll('.keyrow .key');
+        const phraseCharacters = this.phrase.split('');
+        const noSpacePhraseCharacters = phraseCharacters.filter(character => character !== ' ');
+        const lettersDisplayed = document.querySelectorAll('.letter');
+
+        keys.forEach(item => {
+            item.addEventListener('click', (e) => {
+                const clickedKey = e.target.textContent.toLowerCase();
+    
+                noSpacePhraseCharacters.forEach((character, index) => {
+                    if (clickedKey === character) {
+                        lettersDisplayed[index].classList.add('show'); 
+                    };
+                });        
+            });
+        });
+    };
 };
 
+export { Phrase };
